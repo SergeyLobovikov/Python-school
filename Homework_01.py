f8a87a713ad4f299592217eb600d8cf5f3646750
Sergey Lobovikov <sergey.lobovikov@gmail.com>
2023-06-03 09:26:58 +0300

    создан файл homework.py

---
+++ Homework_01.py
@@ -0,0 +1,16 @@
+﻿# Python 02.06.2023
+# TODO list end tuple
+# TODO условный оператор if
+# TODO - операторы цикла for end while
+# TODO - лаконичная запись цикла
+
+# Типы данных
+x = 1 # число
+word = "Hello"
+
+# Изменяемый массив
+shop_list = ['картошка','капуста','морковь'] # список
+
+# Список методов
+from pprint import pprint
+pprint(dir(shop_list))
